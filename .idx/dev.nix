7d4542c8305fbc594211d7b1deac0ef135ac4404
Sylvester Das <sylvester.das@gmail.com>
2025-08-24 08:31:13 +0000

    feat: only pnpm env migration

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,8 +6,8 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
+    pkgs.corepack_22
     pkgs.zulu
-    pkgs.apt
   ];
   # Sets environment variables in the workspace
   env = {};
