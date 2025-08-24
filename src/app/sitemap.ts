0318a4f7dbad4365717143eb86a74a3a617c392a
Sylvester Das <sylvester.das@gmail.com>
2025-08-24 07:40:03 +0000

    thanks, I need sitemap and robots in this site.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { type MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://www.jaqilinmakeover.com';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
