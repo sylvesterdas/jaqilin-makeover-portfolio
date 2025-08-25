9c17763c8938d2fbb8d98451da818af6afae3a2d
Sylvester Das <sylvester.das@gmail.com>
2025-08-25 14:01:46 +0000

    See more info here: https://nextjs.org/docs/messages/react-hydration-err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,7 +117,7 @@ export default function RootLayout({
   };
 
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
